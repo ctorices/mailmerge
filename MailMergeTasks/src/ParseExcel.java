abdbba228c27886a1585156418cc7272410a0e0e
amandamc72 <amccart8@emich.edu>
2016-03-16 18:45:02 -0400

    Email send test code

--- MailMergeTasks/src/ParseExcel.java
+++ MailMergeTasks/src/ParseExcel.java
@@ -1,6 +1,8 @@
+import java.awt.List;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -11,16 +13,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ParseExcel {
 	
 	public static void main(String [] args){
+		
+		ArrayList<String[]> excelData = new ArrayList<String[]>();
+	
 		try {
 
 			File myFile = new File("Book1.xlsx"); 
 			FileInputStream spreadSheet = new FileInputStream(myFile);
 
-			XSSFWorkbook myWorkBook = new XSSFWorkbook (spreadSheet);
+			XSSFWorkbook myWorkBook = new XSSFWorkbook(spreadSheet);
 			
 			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
 			Iterator<Row> rowIterator = mySheet.iterator();
-			
 			
 			while (rowIterator.hasNext()) { 
 				Row row = rowIterator.next();
@@ -29,7 +33,9 @@ public class ParseExcel {
 					Cell cell = cellIterator.next();
 					switch (cell.getCellType()) {
 						case Cell.CELL_TYPE_STRING: 
-							System.out.print(cell.getStringCellValue() + "\t"); break; 
+							//System.out.print(cell.getStringCellValue() + "\t");  
+							excelData.add(new String[] {cell.getStringCellValue()});
+							break;
 						case Cell.CELL_TYPE_NUMERIC: 
 							System.out.print(cell.getNumericCellValue() + "\t"); break;
 						case Cell.CELL_TYPE_BOOLEAN: 
