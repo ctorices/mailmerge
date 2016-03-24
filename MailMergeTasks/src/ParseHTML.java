48498613ef95dd6a733af54b3128cb2f2f6dcb7e
amandamc72 <amccart8@emich.edu>
2016-03-23 22:15:34 -0400

    Stack Implemented

--- MailMergeTasks/src/ParseHTML.java
+++ MailMergeTasks/src/ParseHTML.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -13,12 +14,30 @@ public class ParseHTML {
 	public static void main(String [] args){
 		
 		File input = new File("Internet2 Tempe Lodging Confirmation Email.html");
+		
 		try {
 			Document doc = Jsoup.parse(input, "UTF-8");
 			String letter = doc.toString();
+			
+			
+			ParseExcel parseExcel = new ParseExcel();
+			ArrayList<ArrayList<String>> data = parseExcel.parseExcelData();
+			//String [] selectedPlaceholders;
+			String email = "pizza.com";		
+			
+			for(int i = 0; i < data.size(); i++){
+				for (int j = 0; j < data.get(i).size(); j++){
+					
+				}
+			}
 
-			letter = letter.replace("firstName", "Amanda");
-			letter = letter.replace("lastName", "McCarty");
+			LetterEmailPair letterEmailPair = new LetterEmailPair(letter,email);
+			Stack theStack = new Stack();
+			
+			theStack.push(letterEmailPair);
+			
+			System.out.println(theStack.toString());
+			
 
 			String outputFile = "filename.html";
 			BufferedWriter htmlWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile)));
