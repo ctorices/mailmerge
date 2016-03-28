46a0d708b38d08e86ab5ba249dd67bd196804d6e
amandamc72 <amccart8@emich.edu>
2016-03-27 22:12:10 -0400

    Insert excel data into letter

--- MailMergeTasks/src/ParseHTML.java
+++ MailMergeTasks/src/ParseHTML.java
@@ -12,32 +12,37 @@ import org.jsoup.nodes.Document;
 public class ParseHTML {
 
 	public static void main(String [] args){
-		
+
 		File input = new File("Internet2 Tempe Lodging Confirmation Email.html");
-		
+
 		try {
-			Document doc = Jsoup.parse(input, "UTF-8");
-			String letter = doc.toString();
 			
-			
+			String letter = "";
+			String email = "";
+
 			ParseExcel parseExcel = new ParseExcel();
 			ArrayList<ArrayList<String>> data = parseExcel.parseExcelData();
-			//String [] selectedPlaceholders;
-			String email = "pizza.com";		
-			
-			for(int i = 0; i < data.size(); i++){
-				for (int j = 0; j < data.get(i).size(); j++){
-					
+			Stack theStack = new Stack();	
+
+			int i = 0;
+			for (int k = 1; k < data.size(); k++){
+
+				Document doc = Jsoup.parse(input, "UTF-8");
+				letter = doc.toString();
+
+				for (int j = 0; j < data.get(k).size(); j++){
+					if (data.get(i).get(j).equalsIgnoreCase("email")){
+						email = data.get(k).get(j);
+					}
+					letter = letter.replace(data.get(i).get(j), data.get(k).get(j));
 				}
+				
+				LetterEmailPair letterEmailPair = new LetterEmailPair(letter,email);
+				theStack.push(letterEmailPair);
 			}
 
-			LetterEmailPair letterEmailPair = new LetterEmailPair(letter,email);
-			Stack theStack = new Stack();
-			
-			theStack.push(letterEmailPair);
-			
 			System.out.println(theStack.toString());
-			
+
 
 			String outputFile = "filename.html";
 			BufferedWriter htmlWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile)));
