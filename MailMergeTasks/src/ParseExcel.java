5a2fd5ea35a988840e2464f35e8452413d2b42e5
amandamc72 <amccart8@emich.edu>
2016-04-03 18:56:00 -0400

    Sending Emails from stack

--- MailMergeTasks/src/ParseExcel.java
+++ MailMergeTasks/src/ParseExcel.java
@@ -10,59 +10,70 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ParseExcel {
-	
+
 	private ArrayList<ArrayList<String>> excelData;
-	
+
 	public ParseExcel(){
 		excelData = new ArrayList<ArrayList<String>>();
 	}
-	
+
 	public ArrayList<ArrayList<String>> parseExcelData(){
-	
+
 		try {
-			
-			File myFile = new File("Book1.xlsx"); 
+
+			File myFile = new File("BulkTest.xlsx"); 
 			FileInputStream spreadSheet = new FileInputStream(myFile);
 
 			XSSFWorkbook myWorkBook = new XSSFWorkbook(spreadSheet);
-			
 			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+				
 			Iterator<Row> rowIterator = mySheet.iterator();
 			int i = -1;
 			while (rowIterator.hasNext()) { 
-				
+
 				Row row = rowIterator.next();
 				i += 1;
-				Iterator<Cell> cellIterator = row.cellIterator(); 
-				excelData.add(new ArrayList<String>());
-				
+				Iterator<Cell> cellIterator = row.cellIterator();
+				if(!isRowEmpty(row)){
+					excelData.add(new ArrayList<String>());
+				}
+
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
 					switch (cell.getCellType()) {
-						case Cell.CELL_TYPE_STRING:
-							excelData.get(i).add(cell.getStringCellValue()); break;
-						case Cell.CELL_TYPE_NUMERIC: 
-							break;
-						case Cell.CELL_TYPE_BOOLEAN: 
-							break; 
-						default : 
+					case Cell.CELL_TYPE_STRING:
+						excelData.get(i).add(cell.getStringCellValue()); break;
+					case Cell.CELL_TYPE_NUMERIC: 
+						excelData.get(i).add(String.valueOf((int)cell.getNumericCellValue())); break;
+					case Cell.CELL_TYPE_BOOLEAN: 
+						break;
+					default : 
 					} 
 				}
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		for(int k = 0; k < excelData.size(); k++){
 			for (int j = 0; j < excelData.get(k).size(); j++){
 				System.out.print(excelData.get(k).get(j) + " ");
 			}
 			System.out.println();
 		}
-		
+
 		return excelData;
 
 	}
-	
+
+	public static boolean isRowEmpty(Row row) {
+		for (int c = row.getFirstCellNum(); c < row.getLastCellNum(); c++) {
+			Cell cell = row.getCell(c);
+			if (cell != null && cell.getCellType() != Cell.CELL_TYPE_BLANK)
+				return false;
+		}
+		return true;
+	}
+
 }
