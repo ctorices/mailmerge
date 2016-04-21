3ed558d2dc028ba12fed84588560fed463e644ac
mrricearoni <duongg4848@gmail.com>
2016-04-21 16:10:47 -0400

    Fixed issue #1 and 2 ParseExcel is now able to deal with date/time entries as well as blank cells

--- MailMergeTasks/src/ParseExcel.java
+++ MailMergeTasks/src/ParseExcel.java
@@ -1,71 +1,59 @@
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ParseExcel {
 
 	private ArrayList<ArrayList<String>> excelData;
-	GUI_DocAndExcelSelect_Frame2 frame2 = new GUI_DocAndExcelSelect_Frame2();
 
-	public ParseExcel(){
+	public ParseExcel(String excel){
 		excelData = new ArrayList<ArrayList<String>>();
 	}
 
-	public ArrayList<ArrayList<String>> parseExcelData(String ExcelPath){
+	public ArrayList<ArrayList<String>> parseExcelData(String excel){
 
 		try {
 
-			File myFile = new File(ExcelPath); 
+			File myFile = new File(excel); 
+			DataFormatter formatData = new DataFormatter();
 			FileInputStream spreadSheet = new FileInputStream(myFile);
 
 			XSSFWorkbook myWorkBook = new XSSFWorkbook(spreadSheet);
 			XSSFSheet mySheet = myWorkBook.getSheetAt(0);
-				
-			Iterator<Row> rowIterator = mySheet.iterator();
-			int i = -1;
-			while (rowIterator.hasNext()) { 
 
-				Row row = rowIterator.next();
-				i += 1;
-				Iterator<Cell> cellIterator = row.cellIterator();
-				if(!isRowEmpty(row)){
+			for (int rn=mySheet.getFirstRowNum(); rn<=mySheet.getLastRowNum(); rn++) {
+				Row row = mySheet.getRow(rn);
+				//If row isn't empty, add new ArrayList to excelData
+				if (!isRowEmpty(row))
+				{
 					excelData.add(new ArrayList<String>());
-				}
-
-				while (cellIterator.hasNext()) {
-					Cell cell = cellIterator.next();
-					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_STRING:
-						excelData.get(i).add(cell.getStringCellValue()); break;
-					case Cell.CELL_TYPE_NUMERIC: 
-						excelData.get(i).add(String.valueOf((int)cell.getNumericCellValue())); break;
-					case Cell.CELL_TYPE_BOOLEAN: 
-						break;
-					default : 
-					} 
+					//Add cell data to current row in ArrayList
+					for (int cn=0; cn<row.getLastCellNum(); cn++) 
+					{
+						Cell cell = row.getCell(cn, Row.RETURN_BLANK_AS_NULL);
+						//If cell is blank, just add blank 
+						if (cell == null) 
+							excelData.get(rn).add(formatData.formatCellValue(cell));
+						else 
+							excelData.get(rn).add(formatData.formatCellValue(cell));
+					}
 				}
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		for(int k = 0; k < excelData.size(); k++){
-			for (int j = 0; j < excelData.get(k).size(); j++){
-				System.out.print(excelData.get(k).get(j) + " ");
-			}
-			System.out.println();
-		}
 		return excelData;
-
 	}
 
 	public static boolean isRowEmpty(Row row) {
